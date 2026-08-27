279f8dd0dfaf9c1c1a8611e6a048818dbd26be7a
hongthai101013-netizen <hongthai101013@gmail.com>
2026-08-27 18:55:47 +0700

    Add navigation bar to admin panel

---
+++ adminh/nav.php
@@ -0,0 +1,13 @@
+<?php
+// Thanh điều hướng (Navigation Bar)
+?>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Admin Panel</a>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item"><a class="nav-link" href="index.php">Trang chủ</a></li>
+      <li class="nav-item"><a class="nav-link" href="listBank.php">Danh sách Ngân hàng</a></li>
+      <li class="nav-item"><a class="nav-link" href="addProducts.php">Thêm Sản phẩm</a></li>
+    </ul>
+  </div>
+</nav>
